18c37e022774b488db9019ee405edec72870d82f
Aaron <gowithwi@gmail.com>
2018-06-05 17:45:42 +0800

    First commit

---
+++ build/CMakeFiles/pid.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/aaron/C_SDC_ND_term2/0604_Pro4_rev1/src/PID.cpp" "/Users/aaron/C_SDC_ND_term2/0604_Pro4_rev1/build/CMakeFiles/pid.dir/src/PID.cpp.o"
+  "/Users/aaron/C_SDC_ND_term2/0604_Pro4_rev1/src/main.cpp" "/Users/aaron/C_SDC_ND_term2/0604_Pro4_rev1/build/CMakeFiles/pid.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "/usr/local/opt/openssl/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ git.sh
@@ -0,0 +1,7 @@
+git init
+git add .
+git commit -m "First commit"
+git remote add origin https://github.com/gowithwi/pid.git
+
+git remote -v
+git push -u origin master
